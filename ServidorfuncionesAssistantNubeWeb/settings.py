415c9cb8648209a98c051637a0a2d1932558c415
OsvaldoVegaOses <osvaldovegaoses@gmail.com>
2024-06-22 22:00:19 -0400

    Update settings.py

--- ServidorfuncionesAssistantNubeWeb/settings.py
+++ ServidorfuncionesAssistantNubeWeb/settings.py
@@ -141,21 +141,21 @@ STATIC_ROOT = '/usr/local/lsws/Example/html/ServidorfuncionesAssistantNubeWeb/pu
 
 
 # Configuración de logging (opcional)
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': os.path.join(BASE_DIR, 'debug.log'),
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
+#LOGGING = {
+#    'version': 1,
+#    'disable_existing_loggers': False,
+#    'handlers': {
+#        'file': {
+#            'level': 'DEBUG',
+#            'class': 'logging.FileHandler',
+#            'filename': os.path.join(BASE_DIR, 'debug.log'),
+#        },
+#    },
+#    'loggers': {
+#        'django': {
+#            'handlers': ['file'],
+#            'level': 'DEBUG',
+#            'propagate': True,
+#        },
+#    },
+#}
